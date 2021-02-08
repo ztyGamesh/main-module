5f60187095293b494de3173342d7eea62dcdf855
zhengtianyang <zhengtianyang@bytedance.com>
2021-02-08 14:08:42 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-08 14:07:57
+ * @LastEditTime: 2021-02-08 14:08:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /main-module/index.js
+ */
+
+module.exports = function () {
+    console.log('hello-world');
+}
